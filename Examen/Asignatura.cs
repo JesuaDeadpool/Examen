d2aca6240b21996a8272de8780e5c0ebb4d6337a
Jesua <144072667+JesuaDeadpool@users.noreply.github.com>
2025-02-19 18:27:13 -0600

    Creacion de la clase Alumno y asignatura Creacion de la Interfaz IAsignatura

---
+++ Examen/Asignatura.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examen
+{
+    public class Asignatura : Alumno, IAsignatura 
+    {
+        public int N1 { get; set; } 
+        public int N2 { get; set; }
+        public int N3 { get; set; }
+        public string NombreAsignatura { get; set; }
+        public string Horario { get; set; }
+        public string NombreDocente { get; set;}
+
+        public double CalcularNotaFinal()
+        {
+            throw new NotImplementedException();
+        }
+
+        public double CalcularNotaFinal(double nota1, double nota2, double nota3)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Imprimir()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string MensajeNotaFinal(double notaFinal)
+        {
+            if(notaFinal >= 0 && notaFinal <= 59)
+            {
+                return "Repobrado"; 
+
+            }else if(notaFinal >= 60 && notaFinal <= 79)
+            {
+                return "Bueno";
+            }else if(notaFinal >= 80 && notaFinal <= 89)
+            {
+                return "Muy Bueno";
+            }else if (notaFinal >= 90 && notaFinal <= 100)
+            {
+                return "Sobresaliente"; 
+            }
+            else
+            {
+                return "";
+            }
+
+          
+        }
+    }
+}
---
+++ Examen/IAsignatura.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examen
+{
+    public interface IAsignatura
+    {
+
+        public double CalcularNotaFinal();
+        public double CalcularNotaFinal(double nota1, double nota2, double nota3);
+
+        public string MensajeNotaFinal(double notaFinal);
+
+        public void Imprimir(); 
+
+
+    }
+}
